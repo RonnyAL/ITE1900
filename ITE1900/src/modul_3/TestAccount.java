d136265bcf7c9e63e1ca83a9228ec02fdd10547c
rlo012 <rlo012@post.uit.no>
2018-02-07 17:47:17 +0100

    -

--- ITE1900/src/modul_3/TestAccount.java
+++ ITE1900/src/modul_3/TestAccount.java
@@ -13,9 +13,9 @@ public class TestAccount {
 		Date d3 = new GregorianCalendar(2011, 9, 7).getTime();
 
 		Account a = new Account("Ronny André Løkken", 1, 18000, d1);
-		Account b = new Account("Wim Felix Mossige", 1, 53000, d1);
-		Account c = new Account("Joakim Christopher Storfjell", 1, 11000, d2);
-		Account d = new Account("Andreas Holm", 1, 19000, d3);
+		Account b = new Account("Wim Felix Mossige", 2, 53000, d1);
+		Account c = new Account("Joakim Christopher Storfjell", 3, 11000, d2);
+		Account d = new Account("Andreas Holm", 4, 19000, d3);
 		
 		Account[] accounts = new Account[]{a, b, c, d};
 		Arrays.sort(accounts);
