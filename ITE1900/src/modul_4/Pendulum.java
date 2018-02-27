897d9ddefdafc798408cc072b2fe478187f59eed
rlo012 <rlo012@post.uit.no>
2018-02-27 15:06:35 +0100

    Finished up Pendulum

--- ITE1900/src/modul_4/Pendulum.java
+++ ITE1900/src/modul_4/Pendulum.java
@@ -1,20 +1,36 @@
 package modul_4;
 
-import javafx.animation.*;
+/* Det ble noe tilfeldig hvilke variabler i Pendulum-klassen som ble klassevariabler.
+ * Dette grunnet mye eksperimentering og litt for lite tid. Leverer likevel som den er
+ * da alt fungerer som det skal! :)
+ */
+
+import javafx.animation.Animation;
+import javafx.animation.Animation.Status;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.*;
-import javafx.scene.shape.*;
-import javafx.scene.text.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Pendulum extends Application {
 
+	private static Stage stage;
 	Circle anchor = new Circle();
 	Circle circle = new Circle();
 	double startAngle = 120;
@@ -22,98 +38,109 @@ public class Pendulum extends Application {
 	DoubleProperty curAngle = new SimpleDoubleProperty(startAngle);
 
 	public void start(Stage primaryStage) {
-		PendulumPane pane = new PendulumPane(); // du må lage klassen PendulumPane
+
+		stage = primaryStage;
+
+		PendulumPane pane = new PendulumPane();
 
 		Scene scene = new Scene(pane, 450, 350);
+		primaryStage.setTitle("Pendulum");
+		primaryStage.setScene(scene);
+		pane.requestFocus();
+		primaryStage.show();
 
-		// circle.centerXProperty().bind(anchor.centerXProperty().add(lineLength).multiply(Math.cos(Math.toRadians(curAngle.getValue()))));
-		// circle.centerYProperty().bind(anchor.centerYProperty().add(lineLength).multiply(Math.sin(Math.toRadians(curAngle.getValue()))));
-
-
-		primaryStage.setTitle("Pendulum"); // Set the stage title
-		primaryStage.setScene(scene); // Place the scene in the stage
-		primaryStage.show(); // Display the stage
-
-//		Timeline animation = new Timeline(new KeyFrame(Duration.millis(100), e -> {
-//			pane.next();
-//		}));
-//		animation.setCycleCount(Timeline.INDEFINITE);
-//		animation.play(); // Start animation
-//
-//		pane.setOnMouseClicked(e -> {
-//			if (animation.getStatus() == Animation.Status.RUNNING) {
-//				animation.pause();
-//			} else {
-//				animation.play();
-//			}
-//		});
-//
-//		pane.requestFocus();
-//		pane.setOnKeyPressed(e -> {
-//			if (e.getCode() == KeyCode.UP) {
-//				animation.setRate(animation.getRate() * 1.1);
-//			} else if (e.getCode() == KeyCode.DOWN) {
-//				animation.setRate(animation.getRate() * 0.9);
-//			}
-//		});
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	public static Stage getStage() {
+		return stage;
+	}
+
 }
 
 class PendulumPane extends Pane {
-	
-	public char direction;
-	public DoubleProperty curAngle = new SimpleDoubleProperty(120);
-	public double lineLength = 5;
-	Circle circle = new Circle();
-	Circle anchor = new Circle();
-	Line line = new Line();
-	Text tLVar = new Text();
-	Timeline animation;
-	
 
-	
+	private char direction = 'R';
+	private double curAngle = 120;
+	private double lineLength = 5;
+	private Circle circle = new Circle();
+	private Circle anchor = new Circle();
+	private Line line = new Line();
+	Text tLVar = new Text();
+	private Timeline animation;
+
 	public PendulumPane() {
-		direction = 'R';
-		
+
+		/*
+		 * Listeners tilknyttet stage width/height sørger for at innholdet i
+		 * applikasjonen skaleres også når animasjonen er satt på pause.
+		 */
+
+		Pendulum.getStage().widthProperty().addListener((obs, oldVal, newVal) -> {
+			if (animation.getStatus() == Status.PAUSED) {
+				Point2D cPos = new Point2D(getWidth() / 2,
+						Math.min(getWidth() - getWidth() / 6, getHeight() - getHeight() / 6));
+				Point2D aPos = new Point2D(anchor.getCenterX(), anchor.getCenterY());
+				lineLength = cPos.distance(aPos);
+				circle.setCenterX(anchor.getCenterX() + lineLength * Math.cos(Math.toRadians(curAngle)));
+				circle.setCenterY(anchor.getCenterY() + lineLength * Math.sin(Math.toRadians(curAngle)));
+			}
+		});
+
+		Pendulum.getStage().heightProperty().addListener((obs, oldVal, newVal) -> {
+			if (animation.getStatus() == Status.PAUSED) {
+				Point2D cPos = new Point2D(getWidth() / 2,
+						Math.min(getWidth() - getWidth() / 6, getHeight() - getHeight() / 6));
+				Point2D aPos = new Point2D(anchor.getCenterX(), anchor.getCenterY());
+				lineLength = cPos.distance(aPos);
+				circle.setCenterX(anchor.getCenterX() + lineLength * Math.cos(Math.toRadians(curAngle)));
+				circle.setCenterY(anchor.getCenterY() + lineLength * Math.sin(Math.toRadians(curAngle)));
+			}
+		});
+
 		anchor.radiusProperty().bind(Bindings.min(heightProperty().divide(50), widthProperty().divide(50)));
 		anchor.centerXProperty().bind(widthProperty().divide(2));
 		anchor.centerYProperty().bind(heightProperty().divide(8));
 
 		circle.radiusProperty().bind(Bindings.min(heightProperty().divide(20), widthProperty().divide(20)));
-		
-		
+
 		line.setStrokeWidth(3);
-		line.strokeWidthProperty()
-				.bind(Bindings.min(widthProperty().divide(180), heightProperty().divide(180)));
+		line.strokeWidthProperty().bind(Bindings.min(widthProperty().divide(180), heightProperty().divide(180)));
 
 		line.startXProperty().bind(anchor.centerXProperty().add(circle.translateXProperty()));
 		line.startYProperty().bind(anchor.centerYProperty().add(circle.translateYProperty()));
 
 		line.endXProperty().bind(circle.centerXProperty());
 		line.endYProperty().bind(circle.centerYProperty());
-		
-		Text tAText = new Text("Angle: ");
-		Text tAVar = new Text();
-		TextFlow tfA = new TextFlow();
-		
-		tAVar.textProperty().bind(curAngle.asString());
-		tfA.getChildren().addAll(tAText, tAVar);
-		
-		Text tLText = new Text("Linelength: ");
-		tLVar = new Text();
-		tLVar.setText(String.valueOf(lineLength));
-		
-		TextFlow tfL = new TextFlow(tLText, tLVar);
-		
-		VBox textStuff = new VBox(tfA, tfL);
-		
-		getChildren().addAll(line, circle, anchor, textStuff);
-		
-		animation = new Timeline(new KeyFrame(Duration.millis(40), e -> {
+
+		StackPane pauseOverlay = new StackPane();
+		pauseOverlay.setStyle("-fx-background-color: rgba(255, 255, 255, 0.5);");
+		pauseOverlay.prefWidthProperty().bind(widthProperty());
+		pauseOverlay.prefHeightProperty().bind(heightProperty());
+
+		Rectangle pauseRect1 = new Rectangle();
+		pauseRect1.setFill(Color.BLACK);
+		Rectangle pauseRect2 = new Rectangle();
+		pauseRect2.setFill(Color.BLACK);
+		HBox pauseIcon = new HBox(pauseRect1, pauseRect2);
+		pauseIcon.spacingProperty().bind(widthProperty().divide(20));
+
+		pauseOverlay.getChildren().addAll(pauseIcon);
+		pauseOverlay.setVisible(false);
+		pauseOverlay.toFront();
+
+		pauseRect1.widthProperty().bind(pauseOverlay.widthProperty().divide(10));
+		pauseRect2.widthProperty().bind(pauseOverlay.widthProperty().divide(10));
+		pauseRect1.heightProperty().bind(pauseOverlay.heightProperty().divide(3));
+		pauseRect2.heightProperty().bind(pauseOverlay.heightProperty().divide(3));
+		pauseIcon.setAlignment(Pos.CENTER);
+
+		getChildren().addAll(line, circle, anchor, pauseOverlay);
+
+		animation = new Timeline(new KeyFrame(Duration.millis(100), e -> {
 			next();
 		}));
 		animation.setCycleCount(Timeline.INDEFINITE);
@@ -122,60 +149,57 @@ class PendulumPane extends Pane {
 		setOnMouseClicked(e -> {
 			if (animation.getStatus() == Animation.Status.RUNNING) {
 				animation.pause();
+				pauseOverlay.setVisible(true);
 			} else {
 				animation.play();
+				pauseOverlay.setVisible(false);
 			}
 		});
 
-		requestFocus();
 		setOnKeyPressed(e -> {
 			if (e.getCode() == KeyCode.UP) {
-				animation.setRate(animation.getRate() * 3);
+				animation.setRate(animation.getRate() * 1.5);
+				Pendulum.getStage().setTitle(String.format("Pendulum - Animation rate: %.2f", animation.getRate()));
 			} else if (e.getCode() == KeyCode.DOWN) {
-				animation.setRate(animation.getRate() * 0.9);
+				animation.setRate(animation.getRate() / 1.5);
+				Pendulum.getStage().setTitle(String.format("Pendulum - Animation rate: %.2f", animation.getRate()));
+
 			}
 		});
-		
-		
+
 	}
 
 	public void next() {
-		
-		
-		
-		if (direction == 'R' && curAngle.doubleValue() > 60) {
-			curAngle.set(curAngle.doubleValue()-0.4);
+
+		if (direction == 'R' && curAngle > 60) {
+			curAngle -= 1;
 			direction = 'R';
-			if (curAngle.doubleValue() == 60)
+			if (curAngle == 60)
 				direction = 'L';
 		} else {
-			curAngle.set(curAngle.doubleValue()+0.4);
+			curAngle += 1;
 			direction = 'L';
-			if (curAngle.doubleValue() == 120)
+			if (curAngle == 120)
 				direction = 'R';
 		}
-		
-		Point2D cPos = new Point2D(getWidth()/2, Math.min(getWidth()-getWidth()/6, getHeight()-getHeight()/6));
+
+		Point2D cPos = new Point2D(getWidth() / 2,
+				Math.min(getWidth() - getWidth() / 6, getHeight() - getHeight() / 6));
 		Point2D aPos = new Point2D(anchor.getCenterX(), anchor.getCenterY());
 		lineLength = cPos.distance(aPos);
 
-		tLVar.setText(String.valueOf(animation.getRate()));
-		
-		animation.setRate(animation.getRate() +0.1);
+		/*
+		 * Hadde lyst å bruke binding her heller enn setCenterX() og setCenterY(), men
+		 * får det ikke til å fungere skikkelig. Lar koden stå kommentert ut, så kan du
+		 * frohåpentligvis si noe om hvorfor den ikke fungerer! :)
+		 * 
+		 */
 
+		// circle.centerXProperty().bind(anchor.centerXProperty().add(lineLength).multiply(Math.cos(Math.toRadians(curAngle))));
+		// circle.centerYProperty().bind(anchor.centerYProperty().add(lineLength).multiply(Math.sin(Math.toRadians(curAngle))));
 
-		
-//		circle.centerXProperty().bind(anchor.centerXProperty().add(lineLength).multiply(Math.cos(Math.toRadians(curAngle.doubleValue()))));
-//		cklircle.centerYProperty().bind(anchor.centerYProperty().add(lineLength).multiply(Math.sin(Math.toRadians(curAngle.doubleValue()))));
-
-		
-		circle.setCenterX(anchor.getCenterX()+lineLength*Math.cos(Math.toRadians(curAngle.doubleValue())));
-		circle.setCenterY(anchor.getCenterY()+lineLength*Math.sin(Math.toRadians(curAngle.doubleValue())));
-		
-		
-		
-		
-		
+		circle.setCenterX(anchor.getCenterX() + lineLength * Math.cos(Math.toRadians(curAngle)));
+		circle.setCenterY(anchor.getCenterY() + lineLength * Math.sin(Math.toRadians(curAngle)));
 
 	}
 
