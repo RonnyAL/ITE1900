1d740f6e70278e96cdfb15c0a4a80fa079cade5d
RonnyAL <ronny_loekken@hotmail.com>
2018-03-16 12:21:26 +0100

    Hm

--- ITE1900/src/lek/GreatestCommonDenominator.java
+++ ITE1900/src/lek/GreatestCommonDenominator.java
@@ -1,6 +1,19 @@
 package lek;
 
-public class GreatestCommonDenominator {
+import javafx.application.*;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Line;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+public class GreatestCommonDenominator extends Application {
+	
+	
+	
 	
 	public static int gcd(int p, int q) {
 		if (q == 0)
@@ -11,9 +24,62 @@ public class GreatestCommonDenominator {
 	}
 
 	public static void main(String[] args) {
-		int p = -394200;
-		int q = 1220430;
-		int d = gcd(p, q);
-		System.out.println("gcd(" + p + ", " + q + ") = " + d);
+		
+		launch(args);
+		
+//		int p = -394200;
+//		int q = 1220430;
+//		int d = gcd(p, q);
+//		System.out.println("gcd(" + p + ", " + q + ") = " + d);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Dialog<Pair<String, String>> dialog = new Dialog<>();
+		dialog.setTitle("Største fellesnevner");
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CLOSE);
+
+		GridPane gridPane = new GridPane();
+		gridPane.setHgap(10);
+		gridPane.setVgap(10);
+
+		TextField num1TF = new TextField();
+
+		TextField num2TF = new TextField();
+		num2TF.setStyle("-fx-opacity: 1.0;");
+
+		gridPane.add(new Label("Tall 1: "), 0, 0);
+		gridPane.add(num1TF, 1, 0);
+		gridPane.add(new Label("Tall 2: "), 0, 1);
+		gridPane.add(num2TF, 1, 1);
+		
+		
+		
+		
+		Text numerator = new Text("5");
+		numerator.xProperty().bind(dialog.xProperty().divide(2));
+		numerator.setFont(Font.font ("Verdana", 20));
+		
+		Text denominator = new Text("2");
+		denominator.xProperty().bind(dialog.xProperty().divide(2));
+		denominator.setFont(Font.font ("Verdana", 20));
+		
+		Line line = new Line();
+		line.startXProperty().bind(numerator.xProperty());
+		
+		VBox fraction = new VBox(numerator, line, denominator);
+		fraction.setSpacing(5);
+		Insets fractionLPadding = new Insets(0);
+		
+		VBox root = new VBox(gridPane, fraction);
+		
+
+		dialog.getDialogPane().setContent(root);
+		dialog.setResizable(true);
+
+		Platform.runLater(() -> num1TF.requestFocus());
+		
+		dialog.showAndWait();
+		
 	}
 }
--- ITE1900/src/modul_5/TowerOfHanoi.java
+++ ITE1900/src/modul_5/TowerOfHanoi.java
@@ -1,5 +1,8 @@
 package modul_5;
 
+import java.math.BigInteger;
+import java.util.*;
+
 import javafx.application.Application;
 import javafx.beans.value.*;
 import javafx.concurrent.Task;
@@ -8,18 +11,29 @@ import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TowerOfHanoi extends Application {
-	/** Main method */
 
-	static String strOutput;
-	static int moveCounter;
-	public static final String VALID_REGEX = "\\d{0,2}";
-	static boolean validInput = false;
-	public static final int maxDisks = 15;
+	private static int moveCounter;
+	private final String VALID_REGEX = "\\d{0,10}";
+	private boolean validInput = false;
+	private final int maxDisks = 20;
+	private static final StringBuilder sb = new StringBuilder();
+	private static final String[] superString = { "\\u2070", "\\u00B9", "\\u00B2", "\\u00B3", "\\u2074", "\\u2075",
+			"\\u2076", "\\u2077", "\\u2078", "\\u2079" };
+	private static final char[] superScript = new char[10];
+	private BigInteger movesRequired;
+	private static String[] complaints = { "No thanks.", "Yuck!", "F*ck that.", "You sadist.",
+			"I'm calling the police.", "Ain't nobody got time for that!", "Do it yourself, man!" };
 
 	public static void main(String[] args) {
+
+		for (int i = 0; i < superString.length; i++) {
+			superScript[i] = (char) Integer.parseInt(superString[i].substring(2), 16);
+		}
+
 		launch(args);
 	}
 
@@ -27,15 +41,15 @@ public class TowerOfHanoi extends Application {
 
 		if (n == 1) { // Stopping condition
 			moveCounter += 1;
-			strOutput += String.format("\r\nMove number %d: \t Move disk %s from %s to %s", moveCounter, n, fromTower,
-					toTower);
+			sb.append(String.format(Locale.ROOT, "\r\nMove number %d: \t Move disk %s from %s to %s", moveCounter, n,
+					fromTower, toTower));
 		}
 
 		else {
 			moveDisks(n - 1, fromTower, auxTower, toTower);
 			moveCounter += 1;
-			strOutput += String.format("\r\nMove number %d: \t Move disk %s from %s to %s", moveCounter, n, fromTower,
-					toTower);
+			sb.append(String.format(Locale.ROOT, "\r\nMove number %d: \t Move disk %s from %s to %s", moveCounter, n,
+					fromTower, toTower));
 			moveDisks(n - 1, auxTower, toTower, fromTower);
 		}
 	}
@@ -50,28 +64,31 @@ public class TowerOfHanoi extends Application {
 
 		TextArea output = new TextArea();
 		output.setEditable(false);
-		output.setStyle(
-				"-fx-background-color: -fx-outer-border, -fx-inner-border, -fx-body-color; -fx-faint-focus-color: transparent; -fx-background-insets: 0, 1, 2; -fx-background-radius: 5, 4, 3;");
-		VBox content = new VBox(input, output);
-		content.setPadding(new Insets(10, 0, 0, 10));
+
+		Text footer = new Text();
+
+		VBox content = new VBox(input, output, footer);
+		content.setPadding(new Insets(10, 10, 10, 10));
 		content.setSpacing(10);
 		Pane pane = new Pane(content);
 
+		input.setPrefWidth(400);
+
 		input.spacingProperty().bind(pane.widthProperty().multiply(0.025));
 
-		pane.widthProperty().addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldVal, Number newVal) {
-				userInput.setPrefWidth(newVal.doubleValue() * 0.7);
-				findMoves.setPrefWidth(newVal.doubleValue() * 0.2);
-
-			}
-		});
+		output.prefWidthProperty().bind(pane.widthProperty().subtract(content.getPadding().getLeft())
+				.subtract(content.getPadding().getRight()));
+		output.prefHeightProperty().bind(pane.heightProperty().multiply(0.8));
+		findMoves.prefWidthProperty()
+				.bind(output.widthProperty().multiply(0.2).subtract(content.spacingProperty().divide(2)));
+		userInput.prefWidthProperty()
+				.bind(output.widthProperty().multiply(0.8).subtract(content.spacingProperty().divide(2)));
+		footer.wrappingWidthProperty().bind(output.widthProperty());
 
 		pane.heightProperty().addListener(new ChangeListener<Number>() {
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldVal, Number newVal) {
-				output.setPrefHeight(newVal.doubleValue() - 60);
+				// output.setPrefHeight(newVal.doubleValue() - 80);
 
 			}
 		});
@@ -80,32 +97,42 @@ public class TowerOfHanoi extends Application {
 			@Override
 			public void handle(ActionEvent e) {
 				if (validInput) {
-					strOutput = "Moves:";
+					sb.setLength(0);
+					sb.append("Moves:");
 					moveCounter = 0;
 
 					userInput.setDisable(true);
 					findMoves.setDisable(true);
 					output.setDisable(true);
-					primaryStage.setTitle("Tower of Hanoi - Working...");
 					userInput.getScene().setCursor(Cursor.WAIT);
+					footer.setText("Working...");
+					long startTime = System.currentTimeMillis();
 
 					Task<Void> diskTask = new Task<Void>() {
 						@Override
 						protected Void call() {
 							moveDisks(Integer.parseInt(userInput.getText()), 'A', 'B', 'C');
-							strOutput += "\r\n\r\nNumber of calls to method: " + moveCounter;
-							output.setText(strOutput);
+							output.setText(sb.toString());
 							return null;
 						}
 
 					};
 
 					diskTask.setOnSucceeded(ev -> {
+						long endTime = System.currentTimeMillis();
+						double duration = (endTime - startTime);
 						userInput.getParent().getParent().getScene().setCursor(Cursor.DEFAULT);
 						userInput.setDisable(false);
 						findMoves.setDisable(false);
 						output.setDisable(false);
-						primaryStage.setTitle("Tower of Hanoi");
+						if (duration < 100) {
+							footer.setText(String.format(Locale.ROOT, "%s move%s performed in %.0f ms.", moveCounter,
+									userInput.getText().equals("1") ? "" : "s", duration));
+						} else {
+							footer.setText(String.format(Locale.ROOT, "%s move%s performed in %.2f seconds.",
+									moveCounter, userInput.getText().equals("1") ? "" : "s", duration / 1000.0));
+						}
+
 					});
 
 					new Thread(diskTask).start();
@@ -125,20 +152,63 @@ public class TowerOfHanoi extends Application {
 				userInput.setPromptText("Number of disks (1-" + maxDisks + ")");
 
 				if (newValue.isEmpty() || newValue.equals("0")) {
+					footer.setText("");
 					validInput = false;
+					findMoves.setDisable(true);
 					userInput.setText("");
 				} else if (!newValue.matches(VALID_REGEX)) {
 					userInput.setText(oldValue);
 				} else {
 					try {
-						if (Integer.parseInt(newValue) <= maxDisks) {
-							validInput = true;
-						} else {
-							userInput.setText(oldValue);
-						}
+						BigInteger bigOne = new BigInteger("1");
+						BigInteger bigTwo = new BigInteger("2");
+						movesRequired = bigTwo.pow(Integer.parseInt(userInput.getText()));
+						movesRequired = movesRequired.subtract(bigOne);
 					} catch (NumberFormatException e) {
 						// Skal ikke forekomme, men for sikkerhets skyld:
 						System.out.println(e.getStackTrace());
+					}
+
+					if (Integer.parseInt(newValue) <= maxDisks) {
+						userInput.setStyle("");
+						validInput = true;
+						findMoves.setDisable(false);
+					} else {
+						userInput.setStyle("-fx-text-fill: #999;");
+						validInput = false;
+						findMoves.setDisable(true);
+					}
+
+					String str = movesRequired.toString();
+
+					if (movesRequired.toString().length() > 9) {
+						int power = movesRequired.toString().length() - 1;
+						ArrayList<Integer> powerDigits = new ArrayList<Integer>();
+
+						while (power > 0) {
+							powerDigits.add(power % 10);
+							power = power / 10;
+						}
+
+						Collections.reverse(powerDigits);
+
+						str = movesRequired.toString().substring(0, 1) + "." + movesRequired.toString().substring(2, 6)
+								+ "×10";
+						for (int i : powerDigits) {
+							str += Character.toString(superScript[i]);
+						}
+
+						Random r = new Random();
+						int rIndex = r.nextInt(complaints.length);
+
+						footer.setText(String.format(Locale.ROOT, "%s disk%s would require %s move%s. %s",
+								userInput.getText(), userInput.getText().equals("1") ? "" : "s", str,
+								userInput.getText().equals("1") ? "" : "s", complaints[rIndex]));
+
+					} else {
+						footer.setText(String.format("%s disk%s will require %d move%s.", userInput.getText(),
+								userInput.getText().equals("1") ? "" : "s", movesRequired,
+								movesRequired.toString().equals("1") ? "" : "s"));
 					}
 
 				}
@@ -148,6 +218,7 @@ public class TowerOfHanoi extends Application {
 		});
 
 		userInput.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
 			@Override
 			public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue,
 					Boolean newPropertyValue) {
@@ -160,9 +231,11 @@ public class TowerOfHanoi extends Application {
 		});
 
 		Scene scene = new Scene(pane, 600, 400);
+		pane.prefWidthProperty().bind(pane.getScene().widthProperty());
 		primaryStage.setTitle("Tower of Hanoi");
 		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
 		primaryStage.show();
-
+		primaryStage.setMinWidth(primaryStage.getWidth());
 	}
 }
